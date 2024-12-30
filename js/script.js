b02cd2416b8ae36ab4779146e886f06798659731
Миша Зуев <mzuew2006@gmail.com>
2024-12-30 03:24:16 +0100

    sending data to the server

--- js/script.js
+++ js/script.js
@@ -224,15 +224,54 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // Forms
 
-  const form = document.querySelectorAll("form");
+  const forms = document.querySelectorAll("form");
+
+  const message = {
+    loading: "Загрузка",
+    success: "Спасибо, мы скоро свяжемся с вами...",
+    failure: "Что-то пошло не так",
+  };
+
+  forms.forEach((item) => {
+    postData(item);
+  });
 
   function postData(form) {
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
 
       const request = new XMLHttpRequest();
-
       request.open("POST", "server.php");
+
+      request.setRequestHeader("Content-type", "application/json");
+      const formData = new FormData(form);
+
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+
+      request.send(json);
+
+      request.addEventListener("load", () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 3000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
     });
   }
 });
