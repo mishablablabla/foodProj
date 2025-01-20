14a31c0dd39a898c4d51a0c3382a2d754f2ae82f
Миша Зуев <mzuew2006@gmail.com>
2025-01-20 02:44:04 +0100

    commonJS modules

---
+++ js/modules/modal.js
@@ -0,0 +1,51 @@
+function modal() {
+  const modalTriger = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal");
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
+  modalTriger.forEach((item) => {
+    item.addEventListener("click", openModal);
+  });
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+
+  modal.addEventListener("click", (event) => {
+    if (
+      event.target == modal ||
+      event.target.getAttribute("data-close") == ""
+    ) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 10000);
+
+  function showModalScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalScroll);
+    }
+  }
+  window.addEventListener("scroll", showModalScroll);
+}
+
+module.exports = modal;
